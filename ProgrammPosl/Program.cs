d18538c7d210da1da2456e910ec99ea47d9c6e7c
Stas Osk <73402405+StasBeep@users.noreply.github.com>
2023-09-13 11:45:16 +0300

    C++ C#

---
+++ ProgrammPosl/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammPosl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите a = ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("a + 5 = ");
+            double b = Math.Sqrt(a) + 5;
+
+            Console.WriteLine(b);
+
+            Console.ReadLine();
+        }
+    }
+}
