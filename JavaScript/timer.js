c66933240a14fa0369b860a4dff519688f2ef07b
Stas Osk <73402405+StasBeep@users.noreply.github.com>
2024-03-04 10:05:06 +0300

    недоделанный таймер

---
+++ JavaScript/timer.js
@@ -0,0 +1,44 @@
+"use strict";
+
+let btn = document.querySelector("#play");
+let timeInp = document.querySelectorAll(".time");
+let texttime = document.querySelector("#textime");
+let timerId;
+let k = false;
+
+let h;
+let m;
+let s;
+
+console.log(btn);
+console.log(timeInp);
+console.log(texttime);
+
+btn.addEventListener('click', playTimer);
+
+function playTimer() {
+    h = timeInp[0].value;
+    m = timeInp[1].value;
+    s = timeInp[2].value;
+    timerId = setInterval(timerChange, 1000);
+}
+
+function timerChange() {
+    s--;
+    k = checkTimer(h, m, s);
+    if (k) {
+        clearInterval(timerId);
+    }
+    changeText(h, m, s);
+}
+
+function changeText(h, m, s) {
+    texttime.innerHTML = `${h} : ${m} : ${s}`;
+}
+
+function checkTimer(h, m, s) {
+    if (h == 0 && m == 0 && s == 0) {
+        return true;
+    }
+    return false;
+}
