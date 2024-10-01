0fa9f24d4c62b6b7d77cd92e25d085cc6cabbf28
Stas Osk <73402405+StasBeep@users.noreply.github.com>
2024-10-01 11:21:16 +0300

    array button

--- src/pages/SecondPage.tsx
+++ src/pages/SecondPage.tsx
@@ -2,9 +2,9 @@ import NavMenu from "../components/NavMenu"
 
 const contentMenu = [
   {
-  product: "button1",
-  id: 212,
-  img: "img"
+    product: "button1",
+    id: 212,
+    img: "img"
   },
   {
     product: "button2",
@@ -26,14 +26,14 @@ const contentMenu = [
 const SecondPage = () => {
   return (
     <div>
-      <NavMenu
-        productNav={contentMenu[0].product}
-        alertFunction={contentMenu[0].id}
-      />
-      <NavMenu
-        productNav={contentMenu[1].product}
-        alertFunction={2}
-      />
+      {
+        contentMenu.map((item) => (
+          <NavMenu
+            productNav={item.product}
+            alertFunction={item.id}
+          />
+        ))
+      }
     </div>
   )
 }
