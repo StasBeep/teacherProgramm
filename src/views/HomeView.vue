79f2e0062ac7ead32325643a01a5ff6a44e4cf22
Stas Osk <73402405+StasBeep@users.noreply.github.com>
2024-10-03 16:20:02 +0300

    описание состояния каждой функции + комментирование console.log

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,23 +11,33 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import Footer from "../components/Footer.vue"
+import { ref } from 'vue'; // Переменные состояния
+import Footer from "../components/Footer.vue" // Компонент, используемый в вёрстке
 name: "HomeView";
 
+// Перменные взаимосвязанные с input, при помощи v-model
 const op1 = ref(0);
 const op2 = ref(0);
 const op3 = ref(0);
 const op4 = ref(0);
 const determination = ref(0);
 
+/**
+ * Функция определения нового массива (функция - реакция на состояние)
+ * Присваивание результата определеителя матрицы 2 на 2 переменной состояния
+ */
 const onChangeMatrix = () => {
-  console.log(op1.value, op2.value, op3.value, op4.value);
+  // console.log(op1.value, op2.value, op3.value, op4.value);
   const matrixArray = [[op1.value, op2.value], [op3.value, op4.value]];
-  console.log(matrixArray);
+  // console.log(matrixArray);
   determination.value = resultDeterminationMatrix(matrixArray);
 }
 
+/**
+ * Функция расчёта определителя матрицы 2 на 2
+ * @param { Array[][] } matrix матрица для расчёта
+ * @return { Number } значение определителя
+ */
 const resultDeterminationMatrix = (matrix) => {
   // Цикл один, потому что двигаемся по столбцам
   let mainDiagonal;
@@ -43,15 +53,20 @@ const resultDeterminationMatrix = (matrix) => {
     }
   }
 
-  console.log(mainDiagonal, secondaryDiagonal)
+  // console.log(mainDiagonal, secondaryDiagonal)
 
   return mainDiagonal + secondaryDiagonal;
 }
 
+/**
+ * Расчёт минора со значением на определённо диагонали матрицы
+ * @param { Number } i номер столбца
+ * @param { Array[][] }matrix матрица для расчёта
+ * @return { Number } результат расчёта минора
+ */
 const resultMinor2to2 = (i, matrix) => {
   return matrix[0][i] * Math.pow(-1, 1 + i + 1) * matrix[1][matrix.length - i - 1];
 }
-
 
 </script>
 
