0d88273cc28760533673648d7a9510fc305354a2
Stas Osk <73402405+StasBeep@users.noreply.github.com>
2024-10-03 13:38:07 +0300

    связь с input

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <main>
-    <input type="number">
+    <input v-model="message" type="number" placeholder="k" @input="onChangeMatrix" />
+    {{ message }}
     <input type="number">
     <input type="number">
     <input type="number">
@@ -9,11 +10,19 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import Footer from "../components/Footer.vue"
 name: "HomeView";
 
+const message = ref(0);
+
+const onChangeMatrix = (event) => {
+  console.log(Number(event.target.value));
+  message.value = event.target.value;
+  console.log("Состояние переменной message: " + message.value)
+} 
+
+
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
