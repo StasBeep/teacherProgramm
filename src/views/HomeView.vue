a8766d8c5a3da9084a8c22d510031210d41ca70a
Stas Osk <73402405+StasBeep@users.noreply.github.com>
2024-10-03 23:52:08 +0300

    стилизация блоков (матрицы 2на2)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,24 @@
 <template>
-  <main>
-    <input v-model="op1" type="number" @input="onChangeMatrix" placeholder="k" />
-    <input v-model="op2" type="number" @input="onChangeMatrix" placeholder="k" />
-    <input v-model="op3" type="number" @input="onChangeMatrix" placeholder="k" />
-    <input v-model="op4" type="number" @input="onChangeMatrix" placeholder="k" />
-    =
-    {{ determination }}
+  <main class="determinant">
+    <figure class="determinant-matrix">
+      <div class="determinant-matrix-bracket" />
+      <div class="determinant-matrix-column">
+        <input class="determinant-matrix-column-inp" v-model="op1" type="number" @input="onChangeMatrix"
+          placeholder="k" />
+        <input class="determinant-matrix-column-inp" v-model="op2" type="number" @input="onChangeMatrix"
+          placeholder="k" />
+      </div>
+      <div class="determinant-matrix-column">
+        <input class="determinant-matrix-column-inp" v-model="op3" type="number" @input="onChangeMatrix"
+          placeholder="k" />
+        <input class="determinant-matrix-column-inp" v-model="op4" type="number" @input="onChangeMatrix"
+          placeholder="k" />
+      </div>
+      <div class="determinant-matrix-bracket" />
+    </figure>
+    <div class="determinant-result">
+      = {{ determination }}
+    </div>
   </main>
   <Footer />
 </template>
@@ -45,7 +58,7 @@ const resultDeterminationMatrix = (matrix) => {
   for (let i = 0; i < matrix.length; i++) {
     switch (i) {
       case 0:
-        mainDiagonal =  resultMinor2to2(i, matrix);
+        mainDiagonal = resultMinor2to2(i, matrix);
         break;
       case 1:
         secondaryDiagonal = resultMinor2to2(i, matrix);
@@ -70,4 +83,64 @@ const resultMinor2to2 = (i, matrix) => {
 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.determinant {
+  display: flex;
+  justify-content: center;
+  margin: 250px auto;
+
+  &-matrix {
+    position: relative;
+
+    &-bracket {
+      position: absolute;
+        right: -15px;
+        top: 0;
+        width: 35px;
+        height: 110px;
+        transform: perspective(2px) rotateY(1deg);
+        border: solid black;
+        border-width: 3px 3px 3px 0;
+        border-radius: 0px 45px 45px 0;
+      
+      &:first-child {
+        position: absolute;
+          right: 240px;
+          top: 0;
+          width: 35px;
+          height: 110px;
+          transform: perspective(2px) rotateY(179deg);
+          border: solid black;
+          border-width: 3px 3px 3px 0;
+          border-radius: 0px 45px 45px 0;
+      }
+    }
+
+    &-column {
+
+      &-inp {
+        width: 100px;
+        font-weight: 500;
+        font-size: 20px;
+        line-height: 16px;
+        padding: 10px;
+        margin-right: 20px;
+        border-radius: 10px;
+        border: 0.3px solid black;
+
+        &:first-child {
+          margin-bottom: 20px;
+        }
+      }
+    }
+  }
+
+  &-result {
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 16px;
+    margin-top: 45px;
+    margin-left: 20px;
+  }
+}
+</style>
